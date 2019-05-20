8f3efc92fffcaea5df6982d3b7e92b0d8662236e
Natalia <nayleeeet@gmail.com>
2019-05-20 21:20:31 +0200

    Fix downloading data about top scores

--- app/src/main/java/com/example/guessthenumbergame/TopResultsActivity.kt
+++ app/src/main/java/com/example/guessthenumbergame/TopResultsActivity.kt
@@ -1,11 +1,9 @@
 package com.example.guessthenumbergame
 
 import android.os.AsyncTask
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.View
-import android.widget.ProgressBar
-import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_top_results.*
 import org.json.JSONArray
 import java.lang.Thread.sleep
@@ -18,35 +16,48 @@ class TopResultsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_top_results)
 
         HttpGet(this@TopResultsActivity).execute()
+
     }
 
-    fun returnToMain(view: View){
+    fun returnToMain(view: View) {
         finish()
     }
 
-    class HttpGet(private var activity: TopResultsActivity)  : AsyncTask<String, String, String>(){
+    fun refresh(view: View) {
+        HttpGet(this@TopResultsActivity).execute()
+    }
 
-        private var text = "TOP 10"
+    class HttpGet(private var activity: TopResultsActivity) : AsyncTask<String, String, String>() {
+
+        private var text = "TOP 10\n"
 
         override fun onPreExecute() {
             super.onPreExecute()
-            activity.progressBar.visibility = View.VISIBLE
+            activity.refreshButton?.visibility = View.INVISIBLE
+            activity.progressBar?.visibility = View.VISIBLE
         }
 
         override fun doInBackground(vararg params: String?): String? {
 
-            sleep(1000)
-            val data = URL("http://hufiecgniezno.pl/br/record.php?f=get)").readText()
-            val json = JSONArray(data)
-            if(data != "" ){
-                var field: JSONArray
-                for (i in 0..json.length()){
-                    field = json.getJSONArray(i)
-                    text += "\t" + i + " " + field.getString(1) + " " + field.getString(2) + "\n"
+            sleep(500)
+
+            try {
+                val data = URL("http://hufiecgniezno.pl/br/record.php?f=get").readText()
+                val json = JSONArray(data)
+                if (data != "") {
+                    var field: JSONArray
+                    for (i in 0..json.length()) {
+                        field = json.getJSONArray(i)
+                        text += "\t" + (i + 1) + " " + field.getString(1) + " " + field.getString(2) + "\n"
+                    }
+                    //wpisać do SQLite
                 }
-                //wpisać do SQLite
+                // else if data == err wypisać z SQLite
+
+            } catch (t: Throwable) {
+                t.printStackTrace()
             }
-            // else if data == err wypisać z SQLite
+
 
 
             return "OK"
@@ -54,8 +65,9 @@ class TopResultsActivity : AppCompatActivity() {
 
         override fun onPostExecute(result: String?) {
             super.onPostExecute(result)
-            activity.progressBar.visibility = View.INVISIBLE
-            activity.resultBoard.text = text
+            activity.progressBar?.visibility = View.INVISIBLE
+            activity.refreshButton?.visibility = View.VISIBLE
+            activity.resultBoard?.text = text
         }
     }
 
